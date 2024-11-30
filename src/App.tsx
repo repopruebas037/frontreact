dd245b94c115e02e2582f62ba09fc8a8a714a963
Johnny794 <jajojoap@unal.edu.co>
2024-11-30 10:29:55 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 //import './App.css';
 import './modules/client/containers/ClientMembership'
 import ClientMembership from './modules/client/containers/ClientMembership';
-
+import ChatbotMenu from './modules/chatbot/containers/ChatbotMenu';
 
 function App() {
   return (
-    <ClientMembership/>
+    <div>
+     {/* <ClientMembership/>*/}
+      <ChatbotMenu/>
+    </div>
     
   );
 }
